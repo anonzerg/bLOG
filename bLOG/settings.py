b5362b50771bcc0cc376d9dc2e47e40346f65100
zerg <anonzerg@proton.me>
2025-03-18 02:39:38 +0330

    crispy, bootstrap and users apps added

--- bLOG/settings.py
+++ bLOG/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "blog.apps.BlogConfig",
-    "users.app.UsersConfig",
+    "users.apps.UsersConfig",
+    "crispy_forms",
+    "crispy_bootstrap4"
 ]
 
 MIDDLEWARE = [
@@ -128,4 +130,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
+LOGIN_REDIRECT_URL = "blogHome"
 
