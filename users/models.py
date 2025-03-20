252385e36d59147b2085216d5d29e92f4ac4215f
zerg <anonzerg@proton.me>
2025-03-20 21:34:43 +0330

    resize profile images to 512 by 512

--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from PIL import Image
 # Create your models here.
 
 class Profile(models.Model):
@@ -9,4 +9,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} Profile"
+
+    def save(self):
+        super().save()
+        image_path = self.image.path
+        image = Image.open(image_path)
+        if image.height > 512 or image.width > 512:
+            image_size = (512, 512)
+            image.thumbnail(image_size)
+            image.save(image_path)
 
