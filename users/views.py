470ed2ae32cd0514c7fbd495463051182afa3591
zerg <anonzerg@proton.me>
2025-03-20 21:16:32 +0330

    add functionality to update user info and profile image

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth import logout
 
 # Create your views here.
@@ -17,12 +17,25 @@ def usersRegisterView(request):
             return redirect("usersLogin")
     else:
         form = UserRegisterForm()
-        context = {"form": form, "title": "register"}
+
+    context = {"form": form, "title": "register"}
     return render(request, "users/register.html", context)
 
 @login_required
 def usersProfileView(request):
-    context = {"title": "profile"}
+    if request.method == "POST":
+        user_update_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_update_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_update_form.is_valid() and profile_update_form.is_valid():
+            user_update_form.save()
+            profile_update_form.save()
+            messages.success(request, f"account info has been updated!")
+            return redirect("usersProfile")
+    else:
+        user_update_form = UserUpdateForm(instance=request.user)
+        profile_update_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {"title": "profile", "user_update_form": user_update_form, "profile_update_form": profile_update_form}
     return render(request, "users/profile.html", context)
 
 def usersLogoutView(request):
